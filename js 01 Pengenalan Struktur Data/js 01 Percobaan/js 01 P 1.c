cc5e5b951df4c01d6da14cb31832f0d3d14514bb
makbarfauzan <144658778+makbarfauzan@users.noreply.github.com>
2024-03-25 20:30:01 +0700

    Add files via upload

---
+++ js 01 Pengenalan Struktur Data/js 01 Percobaan/js 01 P 1.c
@@ -0,0 +1,9 @@
+// js 01 P 1.c
+// Percobaan 1 program pertama bahasa C
+
+#include <stdio.h>
+int main()
+{
+	printf("\n Welcome to the world of C "); // prints the message on the screen
+	return 0;// return a value 0 to the operating system
+}
